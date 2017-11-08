ee07098dd9189f6763257f8310fd158228ed8716
Kevin Newman <Kevin@unFocus.com>
2017-11-08 16:55:48 -0500

    restrict posting message to logged in users

--- imports/api/methods/Messages.js
+++ imports/api/methods/Messages.js
@@ -5,8 +5,14 @@ import Messages from '/imports/api/collections/Messages'
 Meteor.methods({
   async addMessage (msg) {
     check(msg, String)
+
+    const userId = Meteor.userId()
+    if (!userId) {
+      throw new Meteor.Error('add-message-not-authenticated', 'You must be logged in to add a message.')
+    }
+
     try {
-      var msgId = await Messages.insert({text: msg})
+      var msgId = await Messages.insert({owner: userId, text: msg})
     } catch (error) {
       throw new Meteor.Error('add-message-failed', error.text)
     }
