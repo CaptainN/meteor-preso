6d7a60011a707cfec9d5140acbef2a4f0fd5231f
Kevin Newman <Kevin@unFocus.com>
2017-11-08 15:40:00 -0500

    add js modern async/await

--- imports/api/methods/Messages.js
+++ imports/api/methods/Messages.js
@@ -3,10 +3,10 @@ import { check } from 'meteor/check'
 import Messages from '/imports/api/collections/Messages'
 
 Meteor.methods({
-  addMessage (msg) {
+  async addMessage (msg) {
     check(msg, String)
     try {
-      var msgId = Messages.insert({text: msg})
+      var msgId = await Messages.insert({text: msg})
     } catch (error) {
       throw new Meteor.Error('add-message-failed', error.text)
     }
