691fb3d28397ffcb9bf8581ae94c7c29158d6ce3
Kevin Newman <Kevin@unFocus.com>
2017-11-08 15:25:52 -0500

    use complete error handling inside method (being mindful of Fibers)

--- imports/api/methods/Messages.js
+++ imports/api/methods/Messages.js
@@ -3,6 +3,11 @@ import Messages from '/imports/api/collections/Messages'
 
 Meteor.methods({
   addMessage (msg) {
-    return Messages.insert({text: msg})
+    try {
+      var msgId = Messages.insert({text: msg})
+    } catch (error) {
+      throw new Meteor.Error('add-message-failed', error.text)
+    }
+    return msgId
   }
 })
